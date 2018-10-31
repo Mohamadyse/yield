e986e3712c06ff81fa998c40d7e4f19c16b8aa09
Mohamad <mohamad.y.s.e@gmail.com>
2018-10-31 07:55:47 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MainClass
+{
+  static IEnumerable <int> WhileMethod(int order)
+    {
+        int previous = 0;
+        int current = 1;
+        for (int j=1; j<=order; j++)
+        {
+            int old = current;
+         yield return  current = current + previous;
+          previous = old;
+          
+        }
+    }
+    static void Main()
+    {
+        foreach(int num in WhileMethod(10))
+            Console.WriteLine(num);
+    }
+
+}
